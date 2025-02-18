1a2e6b72c9670627bdaa9fc9b00a9846830de2b3
OMR-Trend <nitheesgopi@gmail.com>
2025-02-18 19:23:57 +0530

    new update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter:"cypress-allure-plugin",
+  reporterOptions:{
+    resultsDir:"allure-results"
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("@shelex/cypress-allure-plugin")(on);
+      return config;
     },
   },
 });
